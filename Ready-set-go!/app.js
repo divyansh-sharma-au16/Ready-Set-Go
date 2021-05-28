039a602544aebc481d788bf07137eed31ff62095
Divyansh Sharma <divyanshsharma7765@gmail.com>
2021-05-29 04:36:23 +0530

    error handling implemented

--- Ready-set-go!/app.js
+++ Ready-set-go!/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/globalErrorHndler');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
 
@@ -16,11 +18,6 @@ app.use(express.json());
 app.use(express.static('./public'));
 
 app.use((req, res, next) => {
-  console.log('hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestedAt = new Date().toISOString();
   next();
 });
@@ -29,5 +26,11 @@ app.use((req, res, next) => {
 
 app.use('/api/V1/users', userRouter);
 app.use('/api/V1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- Ready-set-go!/models/tourModel.js
+++ Ready-set-go!/models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = mongoose.Schema(
   {
@@ -7,7 +9,11 @@ const tourSchema = mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal to 40 charachters'],
+      minlength: [10, 'A tour name must have more or equal to 10 charachters'],
+      // validate: [validator.isAlpha, 'the name must only contain alphabets'],
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -19,10 +25,16 @@ const tourSchema = mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour mus have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty must be either easy , medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be more than 1.0'],
+      max: [5, 'Rating must be less than 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -32,7 +44,15 @@ const tourSchema = mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Disscount price({VALUE}) should be less than regular price)',
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -53,6 +73,10 @@ const tourSchema = mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -62,6 +86,33 @@ const tourSchema = mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// Document middleware
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Query Middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`query took ${Date.now() - this.start} milliseconds`);
+  next();
+});
+
+// Aggregation Middleware
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
